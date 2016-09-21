728a022604b61fc52ae9edfdb6170fe3ffa5762a
Illyoung Choi <iychoi@email.arizona.edu>
2016-09-21 14:47:18 -0700

    Fix typo

--- syndicate.js
+++ syndicate.js
@@ -801,7 +801,7 @@ module.exports = {
             return;
         }
 
-        if(!path) {
+        if(!fh) {
             callback(new Error("Invalid arguments"), null);
             return;
         }
